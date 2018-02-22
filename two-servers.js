4a21c6f5690626336e1cab75cfe8055ad1ca22d3
JoJo Zheng^C <jojozheng1216@hotmail.com>
2018-02-21 19:22:53 -0700

    added all functions

--- two-servers.js
+++ two-servers.js
@@ -1,21 +1,27 @@
 const http= require('http');
 
-cost PORT= 7000;
-cost PORTTWO= 7500;
+const PORT= 7000;
+const PORTTWO= 7500;
 
 var handleRequest= function(request, response){
-	const.log(`This works! Path hit: ${request.url}`);
-}
+	console.log(`You're good!`);
+	response.end(`This works! Path hit: ${request.url}  You are beautiful!`);
+};
+
+var handleRequest2= function(request, response){
+	console.log(`You're not good!`);
+	response.end(`This works! Path hit: ${request.url}  You are ugly!`);
+};
 
 var server= http.createServer(handleRequest);
-var server2= http.createServer(handleRequest);
+var server2= http.createServer(handleRequest2);
 
 server.listen(PORT, function(){
-	console.log(`Listening on http://localhost: ${PORT}. You are beautiful!`);
+	console.log(`Listening on http://localhost:${PORT}.`);
 
 });
 
 server2.listen(PORTTWO, function(){
-	console.log(`Listening on http://localhost: ${PORTTWO}. You are ugly!`);
+	console.log(`Listening on http://localhost:${PORTTWO}.`);
 
 });
